b5dd738d90a25d2923b051607fc5e59a1f3ec1f9
Vignesh <vigneshbaskaran7531@gmail.com>
2026-04-19 18:24:05 +0530

    Server test

--- backend/server.js
+++ backend/server.js
@@ -8,11 +8,16 @@ const registerRoutes = require("./routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: ["https://b9cc-2401-4900-cac9-c505-e585-f820-cce4-739c.ngrok-free.app"],
+  credentials: true
+}));
 
 app.use(bodyParser.json());
 
-db.connectDB();
+app.get("/", (req, res) => res.send("Hi"));
+
+// db.connectDB();
 
 registerRoutes(app);
 
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    allowedHosts: [
+      'b9cc-2401-4900-cac9-c505-e585-f820-cce4-739c.ngrok-free.app'
+    ]
+  }
 })
