403b6126aa2301f63ca609855c500bebcd8fe065
Vignesh <vigneshbaskaran7531@gmail.com>
2026-04-19 19:37:27 +0530

    Cors fixed1

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const registerRoutes = require("./routes");
 const app = express();
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://lingo-six-sigma.vercel.app/"],
+  origin: ["http://localhost:5173", "https://lingo-six-sigma.vercel.app"],
   credentials: true
 }));
 
